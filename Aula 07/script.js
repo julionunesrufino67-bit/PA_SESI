1afd35260f04cf7d787f3c584228cc92cc8a9743
Júlio <julionunesrufino26@gmail.com>
2026-06-02 09:12:40 -0300

    Aula 07

---
+++ Aula 07/script.js
@@ -0,0 +1,10 @@
+let nome = document .querySelector('nome').value
+
+function exibirNome(){
+    alert(nome)
+}
+function somar(){
+    let valA = Number(document.querySelector('#ValorA') .value)
+    let valB = Number(document.querySelector('#ValorB') .value)
+    alert(valA+valB)
+}
